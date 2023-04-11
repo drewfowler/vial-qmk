3d8432e2300d35cd3767bbbd2cd3913ad9cae371
DrewF <drewfowler17@gmail.com>
2023-04-11 09:58:56 -0600

    add chocofi

--- keyboards/minisub/keymaps/default/keymap.c
+++ keyboards/minisub/keymaps/default/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
     KC_TRNS, KC_MINS, KC_EQL,  KC_BSLS, KC_TRNS, KC_TRNS, KC_UP,   KC_LEFT, KC_DOWN, KC_RGHT, KC_SCLN, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_SLSH, KC_PGUP, KC_BSLS,
-    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_HOME, KC_PGDN, KC_END
+    RESET, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_HOME, KC_PGDN, KC_END
   ),
   [2] = LAYOUT( /* LAYER 1 */
     KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MPLY,
@@ -28,3 +28,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
+ 
